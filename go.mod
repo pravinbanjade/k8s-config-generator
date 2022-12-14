6323c5de4505f13be285b8cc04d11a6bacbc285c
Pravin Banjade <pravinbanjade9@gmail.com>
2022-12-14 13:12:37 +0545

    previous code

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/pravinbanjade/k8s-config-generator
 
 go 1.19
 
-require github.com/otiai10/copy v1.9.0
-
 require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
--- main.go
+++ main.go
@@ -1,70 +1,50 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
-
-	cp "github.com/otiai10/copy"
+	"path/filepath"
 )
 
 func getUserInput() {
 
+	var appName, imageName, imageTagProd, imageTagStage, containerPort, ingressHostProd, ingressSecretKeyProd, ingressHostStage, ingressSecretKeyStage string
 	// Get Namespace / App Name
 	fmt.Println("Enter the namespace for the app (excluding prod/stage):")
-	inputReader := bufio.NewReader(os.Stdin)
-	appName, _ := inputReader.ReadString('\n')
-	appName = strings.TrimSuffix(appName, "\n")
+	fmt.Scan(&appName)
 
 	// Get Image name
 	fmt.Println("Enter docker image name (without tags):")
-	inputReader = bufio.NewReader(os.Stdin)
-	imageName, _ := inputReader.ReadString('\n')
-	imageName = strings.TrimSuffix(imageName, "\n")
+	fmt.Scan(&imageName)
 
 	// Get Image name
 	fmt.Println("Enter docker image production tag (Eg: production-311e2b7a):")
-	inputReader = bufio.NewReader(os.Stdin)
-	imageTagProd, _ := inputReader.ReadString('\n')
-	imageTagProd = strings.TrimSuffix(imageTagProd, "\n")
+	fmt.Scan(&imageTagProd)
 
 	// Get Image name
 	fmt.Println("Enter docker image staging tag (Eg: staging-3a8217e2):")
-	inputReader = bufio.NewReader(os.Stdin)
-	imageTagStage, _ := inputReader.ReadString('\n')
-	imageTagStage = strings.TrimSuffix(imageTagStage, "\n")
+	fmt.Scan(&imageTagStage)
 
 	// Get Port
 	fmt.Println("Enter docker container port (Eg: 3000):")
-	inputReader = bufio.NewReader(os.Stdin)
-	containerPort, _ := inputReader.ReadString('\n')
-	containerPort = strings.TrimSuffix(containerPort, "\n")
+	fmt.Scan(&containerPort)
 
 	// Get Domain Name: Production
 	fmt.Println("Enter Domain name for Production Ingress (Eg: prod.example.com):")
-	inputReader = bufio.NewReader(os.Stdin)
-	ingressHostProd, _ := inputReader.ReadString('\n')
-	ingressHostProd = strings.TrimSuffix(ingressHostProd, "\n")
+	fmt.Scan(&ingressHostProd)
 
 	// Get Secret Name: Production
 	fmt.Println("Enter Secret Key for Production Ingress (Eg: k8s-tls-secret-replica):")
-	inputReader = bufio.NewReader(os.Stdin)
-	ingressSecretKeyProd, _ := inputReader.ReadString('\n')
-	ingressSecretKeyProd = strings.TrimSuffix(ingressSecretKeyProd, "\n")
+	fmt.Scan(&ingressSecretKeyProd)
 
 	// Get Domain Name: Staging
 	fmt.Println("Enter Domain name for Staging Ingress (Eg: stage.example.com):")
-	inputReader = bufio.NewReader(os.Stdin)
-	ingressHostStage, _ := inputReader.ReadString('\n')
-	ingressHostStage = strings.TrimSuffix(ingressHostStage, "\n")
+	fmt.Scan(&ingressHostStage)
 
 	// Get Secret Name: Staging
 	fmt.Println("Enter Secret Key for Staging Ingress (Eg: k8s-tls-secret-replica):")
-	inputReader = bufio.NewReader(os.Stdin)
-	ingressSecretKeyStage, _ := inputReader.ReadString('\n')
-	ingressSecretKeyStage = strings.TrimSuffix(ingressSecretKeyStage, "\n")
+	fmt.Scan(&ingressSecretKeyStage)
 
 	replaceText(appName)
 	fmt.Println("Your docker image name is:", imageName)
@@ -78,21 +58,48 @@ func getUserInput() {
 
 }
 
-func replaceText(appName string) {
-	path, err := os.Getwd()
+func copyDir(src, dest string) error {
+	files, err := ioutil.ReadDir(src)
 	if err != nil {
-		panic(err)
+		return err
 	}
-	path = path + "/generated-k8s-config"
-	error := cp.Copy("./template", path)
-	fmt.Println(error)
-	fmt.Println("Your app name is:", appName)
-	filePath := "./base/service-account.yaml"
-	fileData, _ := ioutil.ReadFile(filePath)
-	fileString := string(fileData)
-	fileString = strings.ReplaceAll(fileString, "<appName>", appName)
-	fileData = []byte(fileString)
-	_ = ioutil.WriteFile(filePath, fileData, 0o600)
+
+	for _, file := range files {
+		srcPath := filepath.Join(src, file.Name())
+		destPath := filepath.Join(dest, file.Name())
+
+		if file.IsDir() {
+			// Create the destination directory
+			if err := os.MkdirAll(destPath, file.Mode()); err != nil {
+				return err
+			}
+
+			// Recursively copy the contents of the directory
+			if err := copyDir(srcPath, destPath); err != nil {
+				return err
+			}
+		} else {
+			// Copy the file to the destination
+			data, err := ioutil.ReadFile(srcPath)
+			if err != nil {
+				return err
+			}
+
+			if err := ioutil.WriteFile(destPath, data, file.Mode()); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func replaceText(appName string) {
+	// Copy the "src" directory to the "dest" directory
+	err := copyDir("src/template", "src/generated-template")
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func main() {
