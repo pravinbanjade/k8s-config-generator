c82e4a360795b46eed1ba03278a564ba03f093a8
Pravin Banjade <pravinbanjade9@gmail.com>
2022-12-15 00:46:40 +0545

    add select on terminal

--- main.go
+++ main.go
@@ -6,9 +6,48 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	"github.com/pterm/pterm"
 )
 
-func main() {
+func chooseTemplate() {
+
+	// Print the menu options
+	pterm.Print("\n")
+	pterm.Info.Println("Please select an template to use: (Default node.js)")
+	pterm.Print("\n")
+
+	options := []string{"1. node.js (next, nuxt, express, etc.)", "2. Laravel (php-fpm with nginx)"}
+
+	selectedOption, _ := pterm.DefaultInteractiveSelect.WithOptions(options).Show()
+	pterm.Info.Printfln("Selected option: %s", pterm.Green(selectedOption))
+
+	switch selectedOption {
+	case "1. node.js (next, nuxt, express, etc.)":
+		nodeConfig()
+	case "2. Laravel (php-fpm with nginx)":
+		laravelConfig()
+	default:
+		nodeConfig()
+	}
+}
+
+func nodeConfig() {
+	fmt.Println("node")
+
+	// Get user input
+	fmt.Println("Enter service name:")
+	var serviceName string
+	fmt.Scanln(&serviceName)
+
+	fmt.Println("Enter app name:")
+	var appName string
+	fmt.Scanln(&appName)
+
+	fmt.Println("Enter target port:")
+	var targetPort string
+	fmt.Scanln(&targetPort)
+
 	// Import template YAML file
 	resp, err := http.Get("https://raw.githubusercontent.com/pravinbanjade/k8s-config-generator/main/template.yaml")
 	if err != nil {
@@ -24,19 +63,6 @@ func main() {
 		return
 	}
 
-	// Get user input
-	fmt.Println("Enter service name:")
-	var serviceName string
-	fmt.Scanln(&serviceName)
-
-	fmt.Println("Enter app name:")
-	var appName string
-	fmt.Scanln(&appName)
-
-	fmt.Println("Enter target port:")
-	var targetPort string
-	fmt.Scanln(&targetPort)
-
 	// Replace placeholders in template with user input
 	yaml := strings.Replace(string(yamlTemplate), "{{serviceName}}", serviceName, -1)
 	yaml = strings.Replace(yaml, "{{appName}}", appName, -1)
@@ -48,4 +74,12 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func laravelConfig() {
+	fmt.Println("laravel")
+}
+
+func main() {
+	chooseTemplate()
 }
